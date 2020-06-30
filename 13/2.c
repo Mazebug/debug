fbead9da0d29a84d380a5a40fb961cc698369b62
sing <1466058569@qq.com>
2020-06-30 17:39:21 +0800

    correct spaceuse in 13-14/1-5.c

--- 13/2.c
+++ 13/2.c
@@ -6,68 +6,68 @@ typedef struct node {
     struct node *next;
 } Node;
 
-    Node *circle_create(int n);
+Node *circle_create(int n);
+
+void count_off(Node *head, int n, int k, int m);
+
+int main() 
+{
+    int n, k, m;
+    scanf("%d%d%d", &n, &k, &m);
+    Node *head = circle_create(n);
+    count_off(head, n, k, m);
+    return 0;
+}
+
+Node *circle_create(int n) 
+{
+    Node *temp, *new_node, *head;
+    int i;
+
+    temp = (Node *) malloc(sizeof(Node));
+    head = temp;
+    head->data = 1;
+
+    for(i = 2; i <= n; i++) {
+        new_node = (Node *) malloc(sizeof(Node));
+        new_node->data = i;
+        temp->next = new_node;
+        temp = new_node;
+    }
+
+    temp->next = head;
+
+    return head;
+}
+
+void count_off(Node *head, int n, int k, int m) 
+{
+    Node *temp, *prev;
+    temp = head;
+    prev = temp;
     
-    void count_off(Node *head, int n, int k, int m);
-
-    int main() 
-    {
-        int n, k, m;
-        scanf("%d%d%d", &n, &k, &m);
-        Node *head = circle_create(n);
-        count_off(head, n, k, m);
-        return 0;
-    }
-
-    Node *circle_create(int n) 
-    {
-        Node *temp, *new_node, *head;
-        int i;
-
-        temp = (Node *) malloc(sizeof(Node));
-        head = temp;
-        head->data = 1;
-
-        for(i = 2; i <= n; i++) {
-            new_node = (Node *) malloc(sizeof(Node));
-            new_node->data = i;
-            temp->next = new_node;
-            temp = new_node;
-        }
-
-        temp->next = head;
-
-        return head;
-    }
-
-    void count_off(Node *head, int n, int k, int m) 
-    {
-        Node *temp, *prev;
-        temp = head;
+    while (temp->data != k) {
         prev = temp;
-        
-        while (temp->data != k) {
+        temp = temp->next;
+    }
+    
+    int count = 0;
+    
+    while (count < n) {
+        for (int i = 1; i < m; i++) {
             prev = temp;
             temp = temp->next;
         }
-        
-        int count = 0;
-        
-        while (count < n) {
-            for (int i = 1; i < m; i++) {
-                prev = temp;
-                temp = temp->next;
-            }
-            count++;
-            printf("%d", temp->data);
-            if(count != n){
-                printf(" ");
-            }
-            Node *p = temp;
-            prev->next = p->next;
-            temp = p->next;
-            free(p);
+        count++;
+        printf("%d", temp->data);
+        if(count != n){
+            printf(" ");
         }
-    
-        return;
+        Node *p = temp;
+        prev->next = p->next;
+        temp = p->next;
+        free(p);
     }
+
+    return;
+}
--- 13/4.c
+++ 13/4.c
@@ -6,77 +6,77 @@ typedef struct node {
     struct node *next;
 } Node;
 
-    Node *circle_create(int n);
+Node *circle_create(int n);
 
-    void count_off(Node *head, int n, int k, int m);
+void count_off(Node *head, int n, int k, int m);
 
-    int main() 
-    {
-        int n, k, m;
-        scanf("%d%d%d", &n, &k, &m);
-        Node *head = circle_create(n);
-        count_off(head, n, k, m);
-        return 0;
+int main() 
+{
+    int n, k, m;
+    scanf("%d%d%d", &n, &k, &m);
+    Node *head = circle_create(n);
+    count_off(head, n, k, m);
+    return 0;
+}
+
+Node *circle_create(int n) 
+{
+    Node *temp, *new_node, *head;
+    int i;
+
+    temp = (Node *) malloc(sizeof(Node));
+    head = temp;
+    head->data = 1;
+
+    for(i = 2; i <= n; i++) {
+        new_node = (Node *) malloc(sizeof(Node));
+        new_node->data = i;
+        temp->next = new_node;
+        temp = new_node;
     }
 
-    Node *circle_create(int n) 
-    {
-        Node *temp, *new_node, *head;
-        int i;
+    temp->next = head;
 
-        temp = (Node *) malloc(sizeof(Node));
-        head = temp;
-        head->data = 1;
+    return head;
+}
 
-        for(i = 2; i <= n; i++) {
-            new_node = (Node *) malloc(sizeof(Node));
-            new_node->data = i;
-            temp->next = new_node;
-            temp = new_node;
+void count_off(Node *head, int n, int k, int m) 
+{
+    Node *temp, *pre;
+    int i;
+    int times;
+    times = 0;
+    temp = head;
+    pre = head;
+    if(k == 1) {
+        for(i = 1; i < n; i++) {
+            pre = pre->next;
         }
-
-        temp->next = head;
-
-        return head;
+    } else {
+        for(i = 1; i < k - 1; i++) {
+            pre = pre->next;
+        }
     }
-
-    void count_off(Node *head, int n, int k, int m) 
-    {
-        Node *temp, *pre;
-        int i;
-        int times;
-        times = 0;
-        temp = head;
-        pre = head;
-        if(k == 1) {
-            for(i = 1; i < n; i++) {
-                pre = pre->next;
+    temp = pre->next;
+    i = 0;
+    while(times < n) {
+        i++;
+        if(i == m){
+            printf("%d", temp->data);
+            times++;
+            Node *p = temp;
+            pre->next = p->next;
+            temp = pre->next;
+            free(p);
+            i = 0;
+            if(times != n) {
+                printf(" ");
             }
         } else {
-            for(i = 1; i < k - 1; i++) {
-                pre = pre->next;
-            }
+            pre = temp;
+            temp = temp->next;
         }
-        temp = pre->next;
-        i = 0;
-        while(times < n) {
-            i++;
-            if(i == m){
-                printf("%d", temp->data);
-                times++;
-                Node *p = temp;
-                pre->next = p->next;
-                temp = pre->next;
-                free(p);
-                i = 0;
-                if(times != n) {
-                    printf(" ");
-                }
-            } else {
-                pre = temp;
-                temp = temp->next;
-            }
-        }
-        
-        return;
     }
+    
+    return;
+}
