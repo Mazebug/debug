e7345572f3f96a8fa48e8544d4a1b4c6f6b79a6c
sing <1466058569@qq.com>
2020-06-30 19:18:19 +0800

    fix a bug in 8/1.c

--- 8/1.c
+++ 8/1.c
@@ -4,22 +4,25 @@
 int main() 
 {
     int n;
+    char names[200][200];
+    char max[200];
+
     scanf("%d\n", &n);
-    char names[n][100];
     
     for(int i = 0; i < n; i++){
-        fgets(names[i], 100, stdin);
-    }
-    char temp[100];
-    for(int i = 0; i < n - 1; i++){
-        for(int j = i; j < n - 1; j++){
-            if(strlen(names[j + 1]) > strlen(names[j])){
-                strcpy(temp, names[j + 1]);
-                strcpy(names[j + 1], names[j]);
-                strcpy(names[j], temp);
-            }
+        int z = 0;
+        while(scanf("%c", &names[i][z]) != EOF && names[i][z] != '\n'){
+            z++;
         }
     }
-    printf("%s", names[0]);
+    strcpy(max, names[0]);
+    for(int i = 0; i < n; i++) {
+        if(strlen(max) < strlen(names[i])) {
+            strcpy(max, names[i]);
+        }
+    }
+    
+    printf("%s", max);
+    
     return 0;
 }
