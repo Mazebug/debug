f6a0eaea737ced441092b7baf48f687dea34b07d
sing <1466058569@qq.com>
2020-06-30 17:44:16 +0800

    correct space use in 1/2-3.c

--- 1/3.c
+++ 1/3.c
@@ -3,36 +3,36 @@
 
 #define EPSILON 1e-7
 
-        double bisection(int p, int q, double (*func)(int, int, double));
-        double f(int p, int q, double x);
-        int main() 
-        {
-            int p;
-            int q;
-            scanf("%d%d", &p, &q);
-            printf("%.4f\n", bisection(p, q, f));
-            return 0;
-        }
+double bisection(int p, int q, double (*func)(int, int, double));
+double f(int p, int q, double x);
+int main() 
+{
+    int p;
+    int q;
+    scanf("%d%d", &p, &q);
+    printf("%.4f\n", bisection(p, q, f));
+    return 0;
+}
 
-        double bisection(int p, int q, double (*func)(int, int, double))
-        {        
-            double a = 20.0;
-            double b = -20.0;
-            double m;
-            while(1){
-                m = (a + b) / 2;
-                double c=func(p, q, m);
-                if(fabs(c) < EPSILON) {
-                    return m;
-                } else if (func(p, q, a) * c < 0){
-                    b = m;
-                } else {
-                    a = m;
-                }
-            }
+double bisection(int p, int q, double (*func)(int, int, double))
+{        
+    double a = 20.0;
+    double b = -20.0;
+    double m;
+    while(1){
+        m = (a + b) / 2;
+        double c=func(p, q, m);
+        if(fabs(c) < EPSILON) {
+            return m;
+        } else if (func(p, q, a) * c < 0){
+            b = m;
+        } else {
+            a = m;
         }
+    }
+}
 
-        double f(int p, int q, double x) 
-        {
-            return p * x + q;
-        }
+double f(int p, int q, double x) 
+{
+    return p * x + q;
+}
