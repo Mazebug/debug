7c0a4b5f22210eafdcabcc80c647e6abb5af3e0c
Mazebug <57025759+Mazebug@users.noreply.github.com>
2020-06-30 19:26:55 +0800

    Update 5.c
    fix a bug

--- 8/5.c
+++ 8/5.c
@@ -5,13 +5,13 @@ int main()
 {
     int n;
     scanf("%d\n", &n);   
-    char name[n][100];
+    char name[n][101];
     
     for (int i = 0; i < n; i++){
-        fgets(name[i], 100, stdin);
+        gets(name[i]);
     }
     
-    char name_max[100];
+    char name_max[101];
     
     strcpy(name_max, name[0]);
 
@@ -19,6 +19,8 @@ int main()
         if(strlen(name_max) < strlen(name[i]))
             strcpy(name_max, name[i]);
     }
+    
     printf("%s", name_max);
+    
     return 0;
 }
