d3b93aa18a8044d643775733ebe41826e0eae185
sing <1466058569@qq.com>
2020-06-30 17:13:40 +0800

    correct space use in 1/1-5.c

--- 1/1.c
+++ 1/1.c
@@ -3,39 +3,41 @@
 
 #define EPSILON 1e-7
 
-    double bisection(int p, int q, double (*func)(int, int, double));
-    double f(int p, int q, double x);
-    int main()
-    {
-        int p;
-        int q;
-        scanf("%d%d", &p, &q);
-        printf("%.4f\n", bisection(p, q, f));
-        return 0;
-    }
+double bisection(int p, int q, double (*func)(int, int, double));
 
-    double bisection(int p, int q, double (*func)(int, int, double))
+double f(int p, int q, double x);
+
+int main()
+{
+    int p;
+    int q;
+    scanf("%d%d", &p, &q);
+    printf("%.4f\n", bisection(p, q, f));
+    return 0;
+}
+
+double bisection(int p, int q, double (*func)(int, int, double))
+{
+    double a = -20;
+    double b = 20;
+    double xp;
+    xp = (a + b) / 2;
+    double c = func(p, q, xp);
+    while(fabs(c) >= EPSILON)
     {
-        double a = -20;
-        double b = 20;
-        double xp;
-        xp = (a + b) / 2;
-        double c = func(p, q, xp);
-        while(fabs(c) >= EPSILON)
+        if((c * func(p, q, a)) < 0)
         {
-            if((c * func(p, q, a)) < 0)
-            {
-                b = xp;
-            } else if (c * func(p, q, b) < 0) {
-                a = xp;
-            }
-            xp = (a + b) / 2;
-            c = func(p, q, xp);
+            b = xp;
+        } else if (c * func(p, q, b) < 0) {
+            a = xp;
         }
-        return xp;
+        xp = (a + b) / 2;
+        c = func(p, q, xp);
     }
+    return xp;
+}
 
-    double f(int p, int q, double x) 
-    {
-        return p * x + q;
-    }
+double f(int p, int q, double x) 
+{
+    return p * x + q;
+}
--- 1/4.c
+++ 1/4.c
@@ -3,53 +3,55 @@
 
 #define EPSILON 1e-7
 
-    double bisection(int p, int q, double (*func)(int, int, double));
-    double f(int p, int q, double x);
-    int main() 
-    {
-        int p;
-        int q;
-        scanf("%d%d", &p, &q);
-        printf("%.4f\n", bisection(p, q, f));
-        return 0;
-    }
+double bisection(int p, int q, double (*func)(int, int, double));
 
-    double bisection(int p, int q, double (*func)(int, int, double)) 
-    {     
-        double a;
-        double b;
-	    double m;
-        a = -20.0;
-        b = 20.0;
-        m = (a + b) / 2;
-        if (fabs(f(p, q, m)) < EPSILON){
-             return m;
-        } else if ((f(p, q, m) < 0 && f(p, q, a) > 0) || (f(p, q, m) > 0 && f(p, q, a) < 0)){
-            while (fabs(f(p, q, m)) >= EPSILON){
-                if((f(p, q, m) < 0 && f(p, q, a) > 0) || (f(p, q, m) > 0 && f(p, q, a) < 0)){
-                    b = m;
-                    m = (a + b) / 2;
-                } else if ((f(p, q, m) < 0 && f(p, q, b) > 0) || (f(p, q, m) > 0 && f(p, q, b) < 0)){
-                    a = m;
-                    m = (a + b) / 2;
-                }
+double f(int p, int q, double x);
+
+int main() 
+{
+    int p;
+    int q;
+    scanf("%d%d", &p, &q);
+    printf("%.4f\n", bisection(p, q, f));
+    return 0;
+}
+
+double bisection(int p, int q, double (*func)(int, int, double)) 
+{     
+    double a;
+    double b;
+    double m;
+    a = -20.0;
+    b = 20.0;
+    m = (a + b) / 2;
+    if (fabs(func(p, q, m)) < EPSILON){
+         return m;
+    } else if ((func(p, q, m) < 0 && func(p, q, a) > 0) || (func(p, q, m) > 0 && func(p, q, a) < 0)){
+        while (fabs(func(p, q, m)) >= EPSILON){
+            if((func(p, q, m) < 0 && func(p, q, a) > 0) || (func(p, q, m) > 0 && func(p, q, a) < 0)){
+                b = m;
+                m = (a + b) / 2;
+            } else if ((func(p, q, m) < 0 && func(p, q, b) > 0) || (func(p, q, m) > 0 && func(p, q, b) < 0)){
+                a = m;
+                m = (a + b) / 2;
             }
-            return m;
-        } else if ((f(p, q, m) < 0 && f(p, q, b) > 0) || (f(p, q, m) > 0 && f(p, q, b) < 0)) {
-            while (fabs(f(p, q, m)) >= EPSILON){
-                if((f(p, q, m) < 0 && f(p, q, a) > 0) || (f(p, q, m) > 0 && f(p, q, a) < 0)){
-                    b = m;
-                    m = (a + b) / 2;
-                } else if ((f(p, q, m) < 0 && f(p, q, b) > 0) || (f(p, q, m) > 0 && f(p, q, b) < 0)){
-                    a = m;
-                    m = (a + b) / 2;
-                }
-            }
-            return m;
         }
+        return m;
+    } else if ((func(p, q, m) < 0 && func(p, q, b) > 0) || (func(p, q, m) > 0 && func(p, q, b) < 0)) {
+        while (fabs(func(p, q, m)) >= EPSILON){
+            if((func(p, q, m) < 0 && func(p, q, a) > 0) || (func(p, q, m) > 0 && func(p, q, a) < 0)){
+                b = m;
+                m = (a + b) / 2;
+            } else if ((func(p, q, m) < 0 && func(p, q, b) > 0) || (func(p, q, m) > 0 && func(p, q, b) < 0)){
+                a = m;
+                m = (a + b) / 2;
+            }
+        }
+        return m;
     }
+}
 
-    double f(int p, int q, double x) 
-    {
-        return p * x + q;
-    }
+double f(int p, int q, double x) 
+{
+    return p * x + q;
+}
