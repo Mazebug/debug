dfafc0a84fe9e4ef9eb44660ef991bae6222873b
sing <1466058569@qq.com>
2020-06-30 18:10:21 +0800

    correct space use in 5/3.c and fix a bug in 5/4.c

--- 5/3.c
+++ 5/3.c
@@ -23,14 +23,14 @@ int main()
         for (k = 0; k < 9 - j; k++) {
             if (strcmp(name[k], name[k + 1]) >= 0) {
                 for (l = 0; l < 20; l++) {  
-                    swap(&name[k][l],&name[k + 1][l]);   
+                    swap(&name[k][l], &name[k + 1][l]);   
                 }
             }
         }
     }
     int m;
     for (m = 0; m < 10; m++) {
-        printf("%s",name[m]);
+        printf("%s", name[m]);
         if(m != 9){
             printf("\n");
         }
